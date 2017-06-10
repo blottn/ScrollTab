ce5cf485c2350e6bb5ba9bbc705158c179c5f391
Nicholas Blott <blottn@tcd.ie>
2017-06-10 13:40:36 +0100

    attempting to change title

--- scroll.js
+++ scroll.js
@@ -3,8 +3,14 @@
         console.log(tab);
     });
 };*/
-
-chrome.tabs.getCurrent( function(tab) {
-    console.log(tab);
+setInterval( function() {
+    var empty_query = {};
+    chrome.tabs.query(empty_query, function(all_tabs) {
+        for (var i = 0; i < all_tabs.length ;i++) {
+            var scroll_title = all_tabs[i].title;
+            scroll_title = scroll_title.substring(1, scroll_title.length) + scroll_title[0];
+            all_tabs[i].title = scroll_title;
+            console.log(all_tabs[i].title);
+        }
     });
-
+}, 2000);
