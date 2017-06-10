cf3ac6d465055371165dda82fbcc77d8e02ed7a2
Nicholas Blott <blottn@tcd.ie>
2017-06-10 21:44:35 +0100

    changed to running code on each individual page

--- scroll.js
+++ scroll.js
@@ -6,17 +6,14 @@
 setInterval( function() {
     var empty_query = {};
     chrome.tabs.query(empty_query, function(all_tabs) {
-        for (var i = 0; i < all_tabs.length ;i++) {
-            var scroll_title = all_tabs[i].title;
-            scroll_title = scroll_title.substring(1, scroll_title.length) + scroll_title[0];
-            all_tabs[i].title = scroll_title;
-
-            //update tabs
-            var empty_update_properties = {};
-            chrome.tabs.update(all_tabs[i].id, empty_update_properties);
+        for (var i = 0 ; i < all_tabs.length ; i++) {
             
-            console.log(all_tabs[i].title);
-
+            var id = all_tabs[i].id;
+            
+            var details = {
+                "file": "tabScroll.js"
+            };
+            chrome.tabs.executeScript(parseInt(id), details);
         }
     });
 }, 2000);
