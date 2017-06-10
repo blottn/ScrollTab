4e63f32eeb9b5079ce37be8e9c0926201ebd4441
Nicholas Blott <blottn@tcd.ie>
2017-06-10 18:22:45 +0100

    tried using updating

--- scroll.js
+++ scroll.js
@@ -10,7 +10,13 @@ setInterval( function() {
             var scroll_title = all_tabs[i].title;
             scroll_title = scroll_title.substring(1, scroll_title.length) + scroll_title[0];
             all_tabs[i].title = scroll_title;
+
+            //update tabs
+            var empty_update_properties = {};
+            chrome.tabs.update(all_tabs[i].id, empty_update_properties);
+            
             console.log(all_tabs[i].title);
+
         }
     });
 }, 2000);
