f5529cde0252181357fc3289e92ef3d99c7978f1
Nicholas Blott <blottn@tcd.ie>
2017-06-17 16:29:25 +0100

    fixed an error with chrome:// pages

--- scroll.js
+++ scroll.js
@@ -5,20 +5,27 @@ setInterval( function() {
             
             var id = all_tabs[i].id;
             
-            var details = {
-                "file": "tabScroll.js"
-            };
-            chrome.tabs.executeScript(parseInt(id), details);
+            if( !(all_tabs[i].url.substring(0,9) == "chrome://")) {
+                var details = {
+                    "file": "tabScroll.js"
+                };
+                chrome.tabs.executeScript(parseInt(id), details);
+            }
         }
     });
 }, 600);
 
 
-//TODO set tab creation event which adds a space to the title and marks a tab as scrollable
 
 chrome.tabs.onCreated.addListener( new function(tab) {
+    console.log("printing form the function in the listener?");
+    print_tab(tab);
     script_details = {
         "file": "tabAdded.js"
     };
     chrome.tabs.executeScript(tab.id, script_details);
 });
+
+function print_tab(tab) {
+    console.log(tab);
+}
---
+++ tabAdded.js
@@ -0,0 +1,1 @@
+console.log("a tab has been added!");
